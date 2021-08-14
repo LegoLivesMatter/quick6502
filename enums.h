3263ad9aef24d3849ec9637d472421a0d8d110e9
Duje Mihanović <duje.mihanovic@skole.hr>
2021-08-14 19:12:34 +0200

    Sort and document all instruction enums

--- enums.h
+++ enums.h
@@ -2,9 +2,9 @@ enum {
 	REG_A,
 	REG_X,
 	REG_Y,
-	REG_PC,
-	REG_STACK,
-	REG_PSTATE
+	REG_PC, /* program counter */
+	REG_STACK, /* stack pointer */
+	REG_PSTATE /* processor state */
 };
 
 enum {
@@ -16,29 +16,36 @@ enum {
 };
 
 enum {
-	INSTRUCTION_LDA_IMMED = 0xa9,
-	INSTRUCTION_LDA_ZP = 0xa5,
-	INSTRUCTION_STA_ABS = 0x8d,
-	INSTRUCTION_STA_ZP = 0x85,
-	INSTRUCTION_STA_ABS_X = 0x9d,
-	INSTRUCTION_CMP_IMMED = 0xc9,
-	INSTRUCTION_LDX = 0xa2,
-	INSTRUCTION_STX = 0x8e,
-	INSTRUCTION_INX = 0xe8,
-	INSTRUCTION_DEX = 0xca,
-	INSTRUCTION_TAX = 0xaa,
-	INSTRUCTION_CPX_IMMED = 0xe0,
-	INSTRUCTION_ADC_IMMED = 0x69,
-	INSTRUCTION_ADC_ZP = 0x65,
-	INSTRUCTION_INC_ZP = 0xe6,
-	INSTRUCTION_BNE = 0xd0,
-	INSTRUCTION_BCS = 0xb0,
-	INSTRUCTION_BCC = 0x90,
-	INSTRUCTION_JSR = 0x20,
-	INSTRUCTION_RTS = 0x60,
-	INSTRUCTION_LDA_ABS_X = 0xbd,
-	INSTRUCTION_BEQ = 0xf0,
-	INSTRUCTION_JMP = 0x4c,
+	/* Arithmetic operations */
+	INSTRUCTION_ADC_IMMED = 0x69, /* ADd with Carry (immediate) */
+	INSTRUCTION_ADC_ZP = 0x65, /* ADd with Carry (zero page) */
+	INSTRUCTION_CMP_IMMED = 0xc9, /* CoMPare accumulator with address */
+	INSTRUCTION_CPX_IMMED = 0xe0, /* ComPare register X with address */
+
+	/* Branching instructions */
+	INSTRUCTION_BCC = 0x90, /* Branch if Carry Clear */
+	INSTRUCTION_BCS = 0xb0, /* Branch if Carry Set */
+	INSTRUCTION_BEQ = 0xf0, /* Branch if EQual */
+	INSTRUCTION_BNE = 0xd0, /* Branch if Not Equal */
+	INSTRUCTION_JMP = 0x4c, /* JuMP to address */
+	INSTRUCTION_JSR = 0x20, /* Jump to SubRoutine */
+	INSTRUCTION_RTS = 0x60, /* ReTurn from Subroutine */
+
+	/* Register operations */
+	INSTRUCTION_DEX = 0xca, /* DEcrement X */
+	INSTRUCTION_INX = 0xe8, /* INcrement X */
+	INSTRUCTION_LDA_ABS_X = 0xbd, /* LoaD value into A (absolute,X) */
+	INSTRUCTION_LDA_IMMED = 0xa9, /* LoaD value into A (immediate) */
+	INSTRUCTION_LDA_ZP = 0xa5, /* LoaD value into A (zero page) */
+	INSTRUCTION_LDX = 0xa2, /* LoaD value into X */
+	INSTRUCTION_STA_ABS = 0x8d, /* STore A into address (absolute) */
+	INSTRUCTION_STA_ABS_X = 0x9d, /* STore A into address (absolute,X) */
+	INSTRUCTION_STA_ZP = 0x85, /* STore A into address (zero page) */
+	INSTRUCTION_STX = 0x8e, /* STore X into address */
+	INSTRUCTION_TAX = 0xaa, /* Transfer A to X */
+	
+	/* Miscellaneous */
+	INSTRUCTION_INC_ZP = 0xe6, /* INCrement value at address (zero page) */
 
 	INSTRUCTION_BRK = 0x00
 };
